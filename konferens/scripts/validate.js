c853a9c9086f00457db83e3927be7b509b69f951
zarkroc <9654120+zarkroc@users.noreply.github.com>
2018-11-19 22:31:10 +0100

    reformat

--- konferens/scripts/validate.js
+++ konferens/scripts/validate.js
@@ -5,17 +5,17 @@ var email;
 var organization;
 var selectedType;
 var message;
-var title
+var title;
 var pattern = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
 
 
 
-window.onload = function() {
+window.onload = function () {
     registrationForm = document.getElementById('registration_form');
     if (registrationForm.addEventListener) {
-        registrationForm.addEventListener("submit", function (event) {validateForm(event)}, false); //For modern browsers
+        registrationForm.addEventListener("submit", function (event) { validateForm(event) }, false); //For modern browsers
     } else if (registrationForm.attachEvent) {
-        registrationForm.attachEvent('onsubmit', function (event) {validateForm(event)}); //For old browsers
+        registrationForm.attachEvent('onsubmit', function (event) { validateForm(event) }); //For old browsers
     }
 };
 
@@ -28,12 +28,12 @@ function validateForm(event) {
     message = registrationForm.field_message.value;
     title = registrationForm.field_pres_title.value;
     selectedType = registrationForm.pres_type.value;
-    let isValidEmail = pattern.test(email); 
+    let isValidEmail = pattern.test(email);
 
-    if (firstName.length === 0){
+    if (firstName.length === 0) {
         alert("Du måste fylla i ett förnamn")
         event.preventDefault();
-        
+
     } else if (lastName.length === 0) {
         alert("Du måste fylla in ett efternamn")
         event.preventDefault();
@@ -49,7 +49,7 @@ function validateForm(event) {
     } else if (message.length > 200) {
         alert("Du har skrivit in ett förlångt meddelande");
         event.preventDefault();
-    } else if (selectedType == "lecture"  || selectedType == "seminar" ) {
+    } else if (selectedType == "lecture" || selectedType == "seminar") {
         if (title.length === 0) {
             alert("Titel måste fyllas i när du valt föreläsning eller semenarium");
             event.preventDefault();
